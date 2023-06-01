9202bc2bafbbc9f4c9d13713073bd72c0dabdd8b
sustainability99 <126771099+sustainability99@users.noreply.github.com>
2023-06-01 18:54:06 +0200

    add react intro

---
+++ 16_react_intro_26_05_2023/src/components/MyComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyComponent = () => {
+    return (
+        <p>
+            This is our first custom --component--MyComponent 
+        </p>
+    )
+}
+
+export default MyComponent;
